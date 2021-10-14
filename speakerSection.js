bc337ab8f78fad129d1c325550fb8629223ebb7c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-10-13 23:29:31 -0300

    fix images

--- speakerSection.js
+++ speakerSection.js
@@ -10,42 +10,42 @@ speakerHeader.appendChild(header)
 
 const speakersList = [
   {
-    image: "/writers/stephen-king.jpg",
+    image: "writers/stephen-king.jpg",
     speakerName: "Stephen King",
     speakerTitle: "The master of horror",
     description:
       " He is the author of nearly 90  books, most of them worldwide bestsellers. His latest novel, BILLY SUMMERS, may even be his best work ever. Also, other favorites .",
   },
   {
-    image: "/writers/Barker.png",
+    image: "writers/Barker.png",
     speakerName: "Clive Barker",
     speakerTitle: "Writer and Filmaker",
     description:
       "Clive Barker is one of the prominent contemporary English writers. Besides writing, he is recognized for his creative input in the domain of filmmaking.",
   },
   {
-    image: "/writers/Koontz.jpg",
+    image: "writers/Koontz.jpg",
     speakerName: "Dean Konntz",
     speakerTitle: "Don't hate me. Mr. Formula",
     description:
       "American author. His novels are billed as suspense thrillers, but frequently incorporate elements of horror, fantasy, science fiction, mystery, and satire.",
   },
   {
-    image: "/writers/Rice.jpg",
+    image: "writers/Rice.jpg",
     speakerName: "Anne Rice",
     speakerTitle: "She's cornered the market on Vampires",
     description:
       "Anne Rice is the author of over 30 books, most recently the Toby O'Dare novels Of Love and Evil, and Angel Time; the memoir, Called Out of Darkness;and her two novels about Jesus",
   },
   {
-    image: "/writers/Gaiman.jpg",
+    image: "writers/Gaiman.jpg",
     speakerName: "Neil Gaiman",
     speakerTitle: "British writer",
     description:
       "The most underrated writer of our generation, Neil Gaimans work has been honoured with many awards internationally, including the Newbery and Carnegie Medals.",
   },
   {
-    image: "/writers/maberry.jpg",
+    image: "writers/maberry.jpg",
     speakerName: "Jonathan Maberry",
     speakerTitle: "New York Times best-selling ",
     description:
